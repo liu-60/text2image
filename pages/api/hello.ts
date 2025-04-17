777a18af0d105e026206866b46093199bae31aa4
Leo Maedirosian <liuling_2760@126.com>
2025-04-17 23:54:19 +0800

    Update hello.ts

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -39,6 +39,19 @@ export default function handler(
     Generated: '20250417T23207Z',
     Timezone: 'Asia/Shanghai',
     Author: 'LL60',
-    URL: 'https://github.com/lanceliao/china-holiday-calender'
+    URL: 'https://github.com/lanceliao/china-holiday-calender',
+    Years: {
+        2025: [
+            {
+                Name: "截止时间",
+                StartDate: "2025-08-09",
+                EndDate: "2025-08-09",
+                Duration: 1,
+                CompDays: [],
+                URL: "https://www.gov.cn/zhengce/content/202411/content_6986382.htm",
+                Memo: "gap一年"
+            }
+        ]
+    }
   })
 }
