8be06837469a18de27e96ed55ec1c77981dddf0e
Leo Maedirosian <liuling_2760@126.com>
2025-04-17 23:28:52 +0800

    Update hello.ts

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -10,22 +10,22 @@ export default function handler(
   res: NextApiResponse<Data>
 ) {
   res.status(200).json({
-    "Name": "DEMO日历",
-    "Version": "1.0",
-    "Generated": "20250417T23207Z",
-    "Timezone": "Asia/Shanghai",
-    "Author": "LL60",
-    "URL": "https://github.com/lanceliao/china-holiday-calender",
-    "Years": {
-        "2025": [
+    Name: "DEMO日历",
+    Version: "1.0",
+    Generated: "20250417T23207Z",
+    Timezone: "Asia/Shanghai",
+    Author: "LL60",
+    URL: "https://github.com/lanceliao/china-holiday-calender",
+    Years: {
+        2025: [
             {
-                "Name": "截止时间",
-                "StartDate": "2025-08-09",
-                "EndDate": "2025-08-09",
-                "Duration": 1,
-                "CompDays": [],
-                "URL": "https://www.gov.cn/zhengce/content/202411/content_6986382.htm",
-                "Memo": "gap一年"
+                Name: "截止时间",
+                StartDate: "2025-08-09",
+                EndDate: "2025-08-09",
+                Duration: 1,
+                CompDays: [],
+                URL: "https://www.gov.cn/zhengce/content/202411/content_6986382.htm",
+                Memo: "gap一年"
             },   
         ]
     }
